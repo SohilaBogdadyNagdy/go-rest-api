ed54aa403dfeeb5a5d6e45e39ce172aa9297d0b5
Sohila <sohila@hitchhiker.io>
2019-09-15 14:44:57 +0200

    - Modify test list all transaction test method

--- tests/listTransactions_test.go
+++ tests/listTransactions_test.go
@@ -1,25 +1,31 @@
 package listTransactions
 
 import (
-	"api/routes"
+	"mux"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
-func TestListAllTransactions(t *testing.T) {
-	req, err := http.NewRequest("GET", "/api/payment/transaction", nil)
-	if err != nil {
-		t.Fatal(err)
+func executeRequest(req *http.Request) *httptest.ResponseRecorder {
+	rr := httptest.NewRecorder()
+	router := mux.Router{}
+	router.ServeHTTP(rr, req)
+
+	return rr
+}
+func checkResponseCode(t *testing.T, expected, actual int) {
+	if expected != actual {
+		t.Errorf("Expected response code %d. Got %d\n", expected, actual)
 	}
-
-	requestRecorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(routes.ListAllTransactions)
-	handler.ServeHTTP(requestRecorder, req)
-
-	if status := requestRecorder.Code; status != http.StatusOK {
-		t.Errorf("handler returned wrong status code: got %v want %v",
-			status, http.StatusOK)
+}
+func TestListAllTransactions(t *testing.T) {
+	baseURL := "http://localhost:8080"
+	req, _ := http.NewRequest("GET", baseURL+"/api/payment/transaction", nil)
+	response := executeRequest(req)
+	checkResponseCode(t, http.StatusOK, response.Code)
+	if body := response.Body.String(); body != "[]" {
+		t.Errorf("Expected an empty array. Got %s", body)
 	}
 
 }
